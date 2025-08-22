9d3e06da2385974f17ce8786a4fc11d538e9fba1
IvanMartyian <Ivan.Martyian@audius.de>
2025-08-22 13:10:48 +0000

    2 commit

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,9 +3,17 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
-
+int a,b = 5;
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    a = -10;
+    bool my_flag;
+    std::cout << "a = "<< a <<std::endl;
+    std::cout << "b = "<< b <<std::endl;
+    std::cout << "a + b = "<< a+b <<std::endl;
+    std::cout << "flag = "<< my_flag <<std::endl;
+    my_flag = true;
+    std::cout << "flag = "<< my_flag <<std::endl;
+    unsigned int positive = a+b;
+    std::cout << "positive = " << positive << std::endl;
     return 0;
 }
