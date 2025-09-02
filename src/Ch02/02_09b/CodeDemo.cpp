7214c76b8b18a56889f01d1109bee16c715d3402
IvanMartyian <Ivan.Martyian@audius.de>
2025-09-02 07:40:01 +0000

    Structure

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,8 +7,19 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character {
+    std::string name;
+    int level;
+    character_role role;
+};
 int main(){
-    
+    game_character buddy;
+    buddy.name = "Karl";
+    buddy.level = 21;
+    buddy.role = character_role::protagonist;
+
+    std::cout << "He born as a " << buddy.name <<" he have a such of power at level " << buddy.level << " and he is a role " << (int) buddy.role << "." << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
